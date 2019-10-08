efb02a6a40a70070a3436b71df8ceb4ac30c0ced
Gabriel Sharpton <3bag@Tricias-MBP.lan1>
2019-10-08 14:10:01 -0500

    working on delete route

--- controllers/workouts.js
+++ controllers/workouts.js
@@ -7,27 +7,49 @@ const Workout = require("../models/workout")
 router.get("/", (req, res) => {
     Workout.find({})
       .then(workout => {
-        res.render("index", { workout });
+        res.render("index",  {workout} );
       })
       .catch(err => console.log(err));
   });
 
+  
+  router.get("/add", (req, res) => res.render ("add"))
+
+
   router.get("/:id", (req, res) => {
-      Workout.findOne({ _id: req.params.id}).then(workout => res.render("edit", workout))
+      Workout.findOne({ _id: req.params.id}).then(workout => res.render("edit", {workout}))
   })
 
-  router.get("/edit/:id", (req, res) => {
-      Workout.findOne({_id: req.params.id }).then(workout => {
-          res.render("edit", workout)
-      })
-  })
+//   router.get("/edit/:id", (req, res) => {
+//       Workout.findOne({_id: req.params.id }).then(workout => {
+//           res.render("edit", {workout})
+//       })
+//   })
 
+  
   //Add a new workout to certain day
-  router.get("/add", (req, res) => {
-      Workout.create(req.body).then(workout => {
-          res.redirect("/", workout)
+  
+  
+  router.put("/:id", (req, res) => {
+    req.body.complete = req.body.complete ? true : false
+    Workout.findByIdAndUpdate({ _id: req.params.id }, req.body, { new: true })
+    
+  });
+
+  router.post('/', (req, res) => {
+    Workout.create(req.body).then(workout => {
+      Workout.find({}).then(workout => {
+        res.render(workout)
       })
+    })
   })
+
+  router.delete("/:id", (req, res) => {
+      Workout.findOneAndRemove( { _id: req.params.id }).then(() =>{
+        res.redirect("/")}
+  )})
+
+
 
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const hbs = require("hbs");
 const parser = require("body-parser");
-
+const methodOverride = require("method-override");
 
 const workout = require("./controllers/workouts");
 const app = express();
 
 
 app.set("view engine", "hbs");
-app.use(parser.urlencoded({ extended: true }));
 
+app.use(parser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 app.use("/", workout);
 
 app.listen(3333, () => console.log("listening on port 3333"));
